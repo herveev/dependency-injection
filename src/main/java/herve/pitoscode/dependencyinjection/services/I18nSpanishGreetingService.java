bd5ee8120958b933cbe37f6be6592cb678a9ae02
Hervé EVODA <herveevoda@gmail.com>
2022-02-17 22:10:39 +0100

    Understanding Spring default profile.

--- src/main/java/herve/pitoscode/dependencyinjection/services/I18nSpanishGreetingService.java
+++ src/main/java/herve/pitoscode/dependencyinjection/services/I18nSpanishGreetingService.java
@@ -4,7 +4,7 @@ package herve.pitoscode.dependencyinjection.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES","default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService{
     @Override
