1fe90416d415a7ea5c309ac26e9192f428a1e9b2
Hervé EVODA <herveevoda@gmail.com>
2022-02-17 21:24:02 +0100

    Using @Primaryfiers annotation to tell spring which bean to inject

--- src/main/java/herve/pitoscode/dependencyinjection/controllers/MyController.java
+++ src/main/java/herve/pitoscode/dependencyinjection/controllers/MyController.java
@@ -1,13 +1,19 @@
 package herve.pitoscode.dependencyinjection.controllers;
 
 
+import herve.pitoscode.dependencyinjection.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
-    public String  sayHello(){
-        System.out.println("Hello world!!!");
 
-        return "Hi folks!";
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String  sayHello(){
+        return greetingService.sayGreeting();
     }
 }
